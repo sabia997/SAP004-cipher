973754c7dcb6820512c8819030b25ad3120d442f
sabia997 <sabi.sm12@gmail.com>
2020-04-12 15:38:23 -0300

    ultima versão

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,9 @@
 const cipher = {
   encode:function criptografar(offset, mensagem){
-      let alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-      let resultado = "";
-      let tamanhoDaPalavra = mensagem.length;
+    let alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let resultado = "";
+    let tamanhoDaPalavra = mensagem.length;
+    if((mensagem !== 0 && mensagem !== null)&& (offset !==0 && offset !== null)){  
       for(let contador = 0; contador<tamanhoDaPalavra; contador ++){
         let letra = mensagem.toUpperCase().charAt(contador);
         if (letra !=" "){
@@ -14,29 +15,35 @@ const cipher = {
         else {
           resultado = resultado + "  ";
         }    
-      }  
+      } 
+    }
+    else{
+      throw new TypeError;
+    }   
   return resultado
   },
-
   decode: function descriptografar(offset,mensagem){
     let alfabeto = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
     let resultado = "";
     let tamanhoDaPalavra = mensagem.length;
-    for(let contador = 0; contador<tamanhoDaPalavra; contador ++){
-      let letra = mensagem.toUpperCase().charAt(contador);
-      if (letra !=" "){
+    if((mensagem !== 0 && mensagem !== null)&& (offset !==0 && offset !== null)){
+     for(let contador = 0; contador<tamanhoDaPalavra; contador ++){
+       let letra = mensagem.toUpperCase().charAt(contador);
+       if (letra !=" "){
           let noAlfabeto = alfabeto.indexOf(letra);
           let deslocando = (noAlfabeto+Number(offset))%26;
           let novaletra = alfabeto.charAt(deslocando);
           resultado = resultado+novaletra;
         }
-      else {
+       else {
         resultado = resultado + "  ";
       }    
     }  
+    }
+    else{
+      throw new TypeError;
+    }  
 return resultado
-},
-      
+},     
 };
 export default cipher;
-  
